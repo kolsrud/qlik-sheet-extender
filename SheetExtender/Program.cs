f102b5bf688c635a799eb2efa16b1018221aca29
Øystein Kolsrud <yko@qlik.com>
2021-10-07 13:25:04 +0200

    Implemented sheet extension.

--- SheetExtender/Program.cs
+++ SheetExtender/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using Qlik.Engine;
+using Qlik.Sense.Client;
 
 namespace SheetExtender
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
+        {
+            var url = "<url>";
+            var appId = "<appId>";
+            var sheetId = "<sheetId>";
+
+            var location = Location.FromUri(url);
+            location.AsNtlmUserViaProxy(certificateValidation: false);
+
+            using (var app = location.App(appId))
+            {
+                var sheet = app.GetObject<Sheet>(sheetId);
+                using (sheet.SuspendedLayout)
+                {
+                    var layoutOptions = new JObject();
+                    sheet.Properties.Rows = 18;
+                    layoutOptions["extendable"] = true;
+                    sheet.Properties.Set("layoutOptions", new AbstractStructure(layoutOptions));
+                    sheet.Properties.Set("height", 150);
+                }
+            }
+        }
 	}
 }
